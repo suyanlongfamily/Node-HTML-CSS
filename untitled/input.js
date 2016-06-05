9901004aa766fcf16008eb0067a79ee04b556bd1
suyanlong <592938410@qq.com>
2016-06-05 15:10:09 +0800

    同步读取文件，和异步读取文件的区别。

--- untitled/input.js
+++ untitled/input.js
@@ -3,5 +3,5 @@ fs.readFile("new1.txt",function (eorr,data) {
     if(eorr) return console.error(eorr);
     console.log(data.toString());    
 });
-
-console.log("程序执行结束");
+console.log("程序执行结束");//先执行这一步，
+console.log("程序执行结束");//先执行这一步，
---
+++ untitled/readFileSync.js
@@ -0,0 +1,10 @@
+/**
+ * Created by suyanlong on 2016/6/5.
+ */
+var file = require('fs');
+
+var data = file.readFileSync('new1.txt');//异步读取文件。
+console.log(data);//需要转化一下，才能显示显示正确的编码格式。
+console.log(data.toString());
+
+
